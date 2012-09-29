0eca46957514042f5fbb8741011227230e3abc42
rubyshot <ispyit@gmail.com>
2012-09-29 17:25:40 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abqsos::Application.config.secret_token = '8ee04970ceccb1ecb22025beda36428561e75c5f10b663cd0ad5ea71a66bca5087ee778605ea16a379b4b9810fbb0e555f41131c45efe7840242d3f6e646e3dc'
